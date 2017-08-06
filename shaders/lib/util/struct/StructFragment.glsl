44b7bc742b664e070a412145d7ce29ebd537a240
jcm2606 <jakemichie97@hotmail.com>
2017-08-06 22:26:22 +1000

    Attempting to fix artifacts with latest driver.

--- shaders/lib/util/struct/StructFragment.glsl
+++ shaders/lib/util/struct/StructFragment.glsl
@@ -22,33 +22,49 @@ struct Fragment {
 void createFragment(inout Fragment fragment, in vec2 texcoord) {
   #ifdef IN_TEX0
     fragment.tex0 = texture2DLod(colortex0, texcoord, 0);
+  #else
+    fragment.tex0 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX1
     fragment.tex1 = texture2DLod(colortex1, texcoord, 0);
+  #else
+    fragment.tex1 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX2
     fragment.tex2 = texture2DLod(colortex2, texcoord, 0);
+  #else
+    fragment.tex2 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX3
     fragment.tex3 = texture2DLod(colortex3, texcoord, 0);
+  #else
+    fragment.tex3 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX4
     fragment.tex4 = texture2DLod(colortex4, texcoord, 0);
+  #else
+    fragment.tex4 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX5
     fragment.tex5 = texture2DLod(colortex5, texcoord, 0);
+  #else
+    fragment.tex5 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX6
     fragment.tex6 = texture2DLod(colortex6, texcoord, 0);
+  #else
+    fragment.tex6 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 
   #ifdef IN_TEX7
     fragment.tex7 = texture2DLod(colortex7, texcoord, 0);
+  #else
+    fragment.tex7 = vec4(0.0, 0.0, 0.0, 1.0);
   #endif
 }
